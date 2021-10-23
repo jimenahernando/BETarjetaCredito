665d9b5186ce7f8f50481362fd119dc068f6f3e0
jimena <jimenahernando@hotmail.com>
2021-10-24 01:37:24 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/TarjetaController.cs
@@ -0,0 +1,104 @@
+﻿using BETarjetaCredito.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BETarjetaCredito.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarjetaController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+        public TarjetaController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<TarjetaController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var lisTarjetas = await _context.TarjetaCredito.ToListAsync();
+                    return Ok(lisTarjetas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET api/<TarjetaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TarjetaController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] TarjetaCredito tarjeta)
+        {
+            try
+            {
+                _context.Add(tarjeta);
+                await _context.SaveChangesAsync();
+                return Ok(tarjeta);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<TarjetaController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] TarjetaCredito tarjeta)
+        {
+            try
+            {
+                if (id != tarjeta.Id)
+                {
+                    return NotFound();
+                }
+
+                _context.Update(tarjeta);
+                await _context.SaveChangesAsync();
+                return Ok(new { message = "La tarjet ha sido actualizada con exito" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<TarjetaController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var tarjeta = await _context.TarjetaCredito.FindAsync(id);
+                if (tarjeta == null)
+                {
+                    return NotFound();
+                }
+
+                _context.TarjetaCredito.Remove(tarjeta);
+                await _context.SaveChangesAsync();
+                return Ok( new {  message = "La tarjeta se ha eliminada con exito" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
